9d45391e1a822c5b355d1f004d2df128deab4f74
Peter <peter@home.net>
2010-11-26 22:23:00 -0500

    SetColumnWidth

--- pyeLauncher/MainFrame.py
+++ pyeLauncher/MainFrame.py
@@ -233,6 +233,13 @@ class AppFrame(wx.Frame):
         
         # resize list view
         self.lstProfiles.Size = (self.btnCopyProfile.Position[0]-20, self.mainPanel.Size[1]-20)
+        
+        varWidth = (self.lstProfiles.Size[0]-150)/3
+        if varWidth > 85 :
+            self.lstProfiles.SetColumnWidth(1, varWidth)
+            self.lstProfiles.SetColumnWidth(2, varWidth)
+            self.lstProfiles.SetColumnWidth(3, varWidth)
+            
 
     def OnNewEclipseConfiguration(self, event):
         event.Skip()
