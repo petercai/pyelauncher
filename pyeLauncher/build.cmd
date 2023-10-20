cf51ea837e6b98b265aa67dcf469607cf1daa694
Peter Cai <petercaica@hotmail.com>
2023-10-20 17:33:36 -0400

    peppermintLauncher.spec

--- pyeLauncher/build.cmd
+++ pyeLauncher/build.cmd
@@ -1,12 +1,7 @@
 
 @echo off
 
-@REM pyi-makespec -F -D -w -X  name.py [other scripts …]
-@REM pyinstaller --clean launcherApp.py
-
 set PYE_HOME=%~dp0
-set ECLIPSE_HOME=C:\dev\eclipse\eclipse-rcp-202209\eclipse.exe
-
 
 if "%1" ==  "" goto :USAGE
 if ""%1"" == ""spec"" goto spec
@@ -14,13 +9,13 @@ if ""%1"" == ""build"" goto build
 if ""%1"" == ""clean"" goto clean
 
 :spec
-set OPTION=-F -D -w --icon=C:\dev\eclipse\eclipse-rcp-202209\eclipse.exe,0 -n pyelauncher 
-@REM %PYTHON_HOME%\python.exe %PYINST_HOME%\building\Makespec.py %OPTION% launcherApp.py
+set OPTION=--clean --onedir --windowed --icon=peppermint.jpg -n peppermintLauncher 
+@REM set OPTION=--clean --onefile --onedir --windowed --icon=peppermint.jpg -n pyelauncher 
 pyinstaller %OPTION% launcherApp.py
 goto END
 
 :build
-pyinstaller pyelauncher.spec
+pyinstaller peppermintLauncher.spec
 goto END
 :clean
 deltree  dist build
